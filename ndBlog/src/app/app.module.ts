e722f07c02902abb1fc6d69aec82e613daeb1524
Gladis NDOUAB'S <gladis.douabalet@web.de>
2018-05-18 11:15:03 +0200

    Updating components: - adds service: ng g s services/posts - adds component: ng g c NewsPost

--- ndBlog/src/app/app.module.ts
+++ ndBlog/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { NewPostComponent } from './new-post/new-post.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostListItemComponent
+    PostListItemComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule
