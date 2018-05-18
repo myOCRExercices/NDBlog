9212c2f7016c677af32c21569b9f7b0b9dd8b294
Gladis NDOUAB'S <gladis.douabalet@web.de>
2018-05-18 11:02:20 +0200

    initalizes firebase into the application

--- ndBlog/src/app/app.component.ts
+++ ndBlog/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IPost, Post } from '../classes/Post';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,17 @@ export class AppComponent {
   Posts: IPost[];
 
   constructor() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAG0gRlgq-qzgptN1BVnzlqAD8R8Phts8w",
+      authDomain: "ndblog-71376.firebaseapp.com",
+      databaseURL: "https://ndblog-71376.firebaseio.com",
+      projectId: "ndblog-71376",
+      storageBucket: "ndblog-71376.appspot.com",
+      messagingSenderId: "603608532478"
+    };
+    firebase.initializeApp(config);
+
     this.Posts = [
       new Post('Angular',
         'Angular (commonly referred to as "Angular 5" or "Angular 2") is a TypeScript-based open-source front-end web application platform led by the Angular Team at Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS.',
